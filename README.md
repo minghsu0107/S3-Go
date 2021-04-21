b0ae13405799a118dab07de0410a703f0d9dc2af
minghsu0107 <minghsu0107@gmail.com>
2021-04-21 11:02:22 +0800

    update syntax

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # S3-Go
 ## Usage
 ```
-S3_ENDPOINT=http://localhost:9000 S3_REGION=us-east-1 AWS_ACCESS_KEY_ID=access123 AWS_SECRET_KEY=secret123 go run main.go mybucket
+S3_ENDPOINT=http://localhost:9000 \
+S3_REGION=us-east-1               \
+AWS_ACCESS_KEY_ID=access123       \
+AWS_SECRET_KEY=secret123          \
+go run main.go mybucket
 ```
--- main.go
+++ main.go
@@ -12,29 +12,33 @@ import (
 
 var (
 	s3Endpoint = os.Getenv("S3_ENDPOINT")
-	s3Region = os.Getenv("S3_REGION")
-	accessKey = os.Getenv("AWS_ACCESS_KEY_ID")
-	secretKey = os.Getenv("AWS_SECRET_KEY")
+	s3Region   = os.Getenv("S3_REGION")
+	accessKey  = os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey  = os.Getenv("AWS_SECRET_KEY")
 )
 
 func main() {
-    if len(os.Args) < 2 {
+	if len(os.Args) < 2 {
 		fmt.Println("you must specify a bucket")
 		return
 	}
-    // Configure to use MinIO Server
-    s3Config := &aws.Config{
-        Credentials:      credentials.NewStaticCredentials(accessKey, secretKey, ""),
-        Endpoint:         aws.String(s3Endpoint),
-        Region:           aws.String(s3Region),
-        DisableSSL:       aws.Bool(true),
-        S3ForcePathStyle: aws.Bool(true),
-    }
-    newSession := session.New(s3Config)
-    s3Client := s3.New(newSession)
+	// Configure to use MinIO Server
+	config := &aws.Config{
+		Credentials:      credentials.NewStaticCredentials(accessKey, secretKey, ""),
+		Endpoint:         aws.String(s3Endpoint),
+		Region:           aws.String(s3Region),
+		DisableSSL:       aws.Bool(true),
+		S3ForcePathStyle: aws.Bool(true),
+	}
+	session, err := session.NewSession(config)
+	if err != nil {
+		fmt.Println("failed to create session", err)
+		return
+	}
+	client := s3.New(session)
 
-    i := 0
-	err := s3Client.ListObjectsPages(&s3.ListObjectsInput{
+	i := 0
+	err = client.ListObjectsPages(&s3.ListObjectsInput{
 		Bucket: &os.Args[1],
 	}, func(p *s3.ListObjectsOutput, last bool) (shouldContinue bool) {
 		fmt.Println("Page,", i)
